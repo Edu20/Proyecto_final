b4872de5ad577c37be5e06fd841879092b780bc4
edu <e.aguirre@itecriocuarto.org.ar>
2023-05-30 19:58:43 -0300

    agregado de los test

--- articulos/tests.py
+++ articulos/tests.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from articulos.models import Articulo
 
-# Create your tests here.
+class ArticuloTests(TestCase):
+    def test_creation_articulo_1(self):
+        usuario = User.objects.create(username="nombre_usuario")
+        articulo = Articulo(titulo="Este es el titulo", subtitulo="subtitulo", cuerpo="cuerpo", autor="autor", fecha="22-02-1995", creador=usuario)
+        # Resto de las aserciones y acciones de prueba
+
+    def test_creation_articulo_2(self):
+        usuario = User.objects.create(username="nombre_usuario")
+        articulo = Articulo(titulo="Este es el titulo", subtitulo="subtitulo", cuerpo="cuerpo", autor="autor", fecha="SERIAL-1", creador=usuario)
+        # Resto de las aserciones y acciones de prueba
